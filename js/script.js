fd7447e4cb28b828a5c25290845c992d348cd2e9
Nino Chimento <saninoc84@gmail.com>
2019-12-18 12:40:47 +0100

    creazioni variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 alert("nino");
+// l programma dovrà chiedere all’utente il numero di chilometri che vuole
+// percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+
+// numero di km
+var distanza = parseInt(prompt("Salve quanti km desidera percorrere?"));
+console.log(distanza);
+// eta viaggiaotre
+var eta = parseInt(prompt("Potrebb indicarmi la su età?"));
+console.log(eta);
+// prezzo biglietto o.21
+var costoAlKm = 0.21
+// junior 20%
+
+// senior 40%
