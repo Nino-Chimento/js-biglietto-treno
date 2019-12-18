2f40cdda1ddde8a0730c81b321460f2c59c5e824
Nino Chimento <saninoc84@gmail.com>
2019-12-18 13:01:39 +0100

    if else creato..errore nella formula del prezzo scontato

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-alert("nino");
+// alert("nino");
 // l programma dovrà chiedere all’utente il numero di chilometri che vuole
 // percorrere e l’età del passeggero.
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
@@ -11,7 +11,17 @@ console.log(distanza);
 var eta = parseInt(prompt("Potrebb indicarmi la su età?"));
 console.log(eta);
 // prezzo biglietto o.21
-var costoAlKm = 0.21
+var costoAlKm = 0.21;
 // junior 20%
+if (eta <= 18) {
+var scontoJunior = ((distanza * 0.21) *20) / 100;
+console.log(scontoJunior);
+} else if (eta>= 65) {
+  var scontoSenior = (distanza*0.21*40/100);
+  console.log(scontoSenior);
+}else {
+  var prezzoPieno = distanza * 0.21;
+  console.log(prezzoPieno);
+}
 
 // senior 40%
